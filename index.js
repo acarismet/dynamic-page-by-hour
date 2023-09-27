e763c22be6df14df9d16ebdf156350256d8d7639
Ismet Acar <ismtacr@gmail.com>
2023-09-27 16:48:17 +0300

    günesin container disina ciktiginda kaybolmasi icin olmayan denemeler

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Get a reference to the container element
 const container = document.getElementById('container');
-container.classList.add('position-relative', 'border', 'border-1','border-primary');
+container.classList.add('position-relative', 'border', 'border-1', 'border-primary');
 
 // Set the maximum width and height for the container
 container.style.width = '60rem';
@@ -9,8 +9,6 @@ container.style.height = '30rem';
 // Create the sun image element
 const sun = document.createElement('img');
 sun.src = "./assets/sun.png";
-//sun.classList.add('position-absolute', 'bottom-0', 'end-0');
-
 sun.alt = "A beautiful image";
 sun.width = 75; // Set the image width
 sun.height = 75; // Set the image height
@@ -24,6 +22,7 @@ function moveSun() {
     let angle = 0;
     const centerX = container.clientWidth / 2;
     const centerY = container.clientHeight;
+    let isInsideContainer = true; // Track whether the sun is inside the container
 
     // Use requestAnimationFrame for smoother animation
     function animate() {
@@ -35,6 +34,19 @@ function moveSun() {
         sun.style.transform = `translate(${x}px, ${y}px)`;
 
         angle += 0.005; // Adjust this value to control the speed of movement (smaller values make it slower)
+
+        // Check if the sun goes out of the container
+        if (x <= 0) {
+            if (isInsideContainer) {
+                isInsideContainer = false;
+                sun.style.visibility = 'hidden'; // Hide the sun
+            }
+        } else {
+            if (!isInsideContainer) {
+                isInsideContainer = true;
+                sun.style.visibility = 'visible'; // Show the sun
+            }
+        }
 
         // Stop the animation when the sun reaches the left-bottom corner
         if (x <= 0) {

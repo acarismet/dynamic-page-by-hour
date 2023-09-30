2b8c6289575789a473a0259d0da0174b599235ae
ISMET ACAR <ismtacr@gmail.com>
2023-09-30 19:35:47 +0300

    Sun and move movement had done. > > Co-authored-by: 113294105+chiturca@users.noreply.github.com

--- test.js
+++ test.js
@@ -12,3 +12,21 @@ function degreesToRadians(degrees) {
 const angleInDegrees = 180; // Replace with your desired angle
 const angleInRadians = degreesToRadians(angleInDegrees);
 console.log(`Angle in radians: ${angleInRadians}`);
+
+
+
+
+function moveMoon() {
+    let position = 0;
+    const animationInterval = setInterval(() => {
+        if (position <= (moonbox.clientHeight - moonImage.clientHeight)) {
+            moonImage.style.bottom = position + 'px';
+            position++;
+        } else {
+            clearInterval(animationInterval);
+        }
+    }, 10);
+}
+
+// Call the moveMoon function to start the animation
+moveMoon();
